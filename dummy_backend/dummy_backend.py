abe98b25f884827067ceeeb58046f392403beb7d
Ali Ibrahim <ali103575@gmail.com>
2023-01-14 14:41:14 +0400

    Added dummy Flask backend

--- dummy_backend/dummy_backend.py
+++ dummy_backend/dummy_backend.py
@@ -1,1 +1,36 @@
-# dummy_backend goes here
+from flask import abort, Flask, jsonify, send_file
+
+app = Flask(__name__)
+
+DUMMY_FILES = dict({
+    "File1": {
+        "display-text": "Display text of file 1"
+    },
+    "File2": {
+        "display-text": "Display text of file 2"
+    },
+    "File3": {
+        "display-text": "Display text of file 3"
+    },
+})
+
+@app.route("/api/files")
+def list_files():
+    return jsonify(DUMMY_FILES)
+
+
+@app.route("/api/download/file/<variant>")
+def download_file(variant=None):
+    if not variant in DUMMY_FILES:
+        abort(404)
+        
+    return send_file(
+        f"./dummy_files/{variant}.txt",
+        as_attachment=True
+    )
+
+
+if __name__ == "__main__":
+    # debug=True will cause flask to send detailed exception traceback in
+    # the response body. this is useful when trying out requests from a browser
+    app.run(host="0.0.0.0", port=3000, debug=True)
